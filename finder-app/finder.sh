bd3a4e52c95455516ac751191e4026742e4476d3
ashwinhr186 <ashwinhr186@Ubuntu.myguest.virtualbox.org>
2023-09-04 22:42:42 -0600

    finder.sh and writer.sh implemented

---
+++ finder-app/finder.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Validate whether the script is run with 2 arguments
+if [ "$#" -ne 2 ]; 
+then
+  echo "Error: Invalid number of arguments !"
+  exit 1
+fi
+
+filesdir="$1"
+searchstr="$2"
+
+# Validate whether the first argument is a directory
+if [ ! -d "$filesdir" ]; 
+then
+  echo "Error: '$filesdir' is not a directory or does not exist."
+  exit 1
+fi
+
+# Initialize counters
+num_of_files=0
+num_of_occurrences=0
+
+# Function to search for lines containing searchstr in a file
+search() {
+  local file="$1"
+  local search="$2"
+  local count=$(grep -c "$search" "$file")
+  num_of_occurrences=$((num_of_occurrences + count))
+}
+
+# functiion to traverse files and directories under $filesdir directory recursively
+traverse() {
+  local dir="$1"
+  for item in "$dir"/*; 
+  do
+    #if item is a file, search for the string
+    if [ -f "$item" ]; 
+    then
+      search "$item" "$searchstr"
+      num_of_files=$((num_of_files + 1))
+    #if item is a directory, traverse the directory
+    elif [ -d "$item" ]; 
+    then
+      traverse "$item"
+    fi
+  done
+}
+
+# Initiate the traversal to $filestr directory
+traverse "$filesdir"
+
+echo "The number of files are $num_of_files and the number of matching lines are $num_of_occurrences"
+
+exit 0
---
+++ finder-app/writer.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Validate whether the script is run with 2 arguments
+if [ "$#" -ne 2 ]; 
+then
+  echo "Error: Invalid number of arguments"
+  exit 1
+fi
+
+writefile="$1"
+writestr="$2"
+
+# Create the directory path if it doesn't exist
+mkdir -p "$(dirname "$writefile")"
+
+# Check if the directory path is created successfully
+if [ ! -d "$(dirname "$writefile")" ]; 
+then
+  echo "Error: Could not create directory '$(dirname "$writefile")'"
+  exit 1
+fi
+
+# Write $writestr to $writefile
+echo "$writestr" > "$writefile"
+
+# Check if the file was created successfully
+if [ ! -f "$writefile" ]; 
+then
+  echo "Error: Could not create or write to '$writefile'"
+  exit 1
+fi
+
+echo "Content $writestr was written to $writefile successfully"
+
+exit 0
