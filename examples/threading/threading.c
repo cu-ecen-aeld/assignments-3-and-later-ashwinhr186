2dbe26c21ca5793885db3f0e208aca8857c038f0
ashwinhr186 <ashwinhr186@Ubuntu.myguest.virtualbox.org>
2023-09-23 12:13:51 -0600

    assignment4-part1-working commit

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <syslog.h>
+#include <string.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -14,6 +16,24 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    unsigned int time_to_sleep = ((struct thread_data*)thread_param)->wait_to_obtain_ms;
+    unsigned int time_to_hold = ((struct thread_data*)thread_param)->wait_to_release_ms;
+    pthread_mutex_t* mutex = ((struct thread_data*)thread_param)->mutex;
+    usleep(time_to_sleep*1000);
+    int ret = pthread_mutex_lock(mutex);
+    if(ret != 0)
+    {
+        syslog(LOG_ERR, "pthread_mutex_lock() failed: %d", ret);
+        return NULL;
+    }
+    usleep(time_to_hold*1000);
+    ret = pthread_mutex_unlock(mutex);
+    if(ret != 0)
+    {
+        syslog(LOG_ERR, "pthread_mutex_unlock() failed: %d", ret);
+        return NULL;
+    }
+    ((struct thread_data*)thread_param)->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +48,14 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
+    struct thread_data* thread_func_args = (struct thread_data*)malloc(sizeof(struct thread_data));
+    thread_func_args->mutex = mutex;
+    thread_func_args->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_func_args->wait_to_release_ms = wait_to_release_ms;
+    int ret = pthread_create(thread, NULL, threadfunc, thread_func_args);
+    if(ret == 0)
+        return true;
+    syslog(LOG_ERR, "pthread_create() failed: %s", strerror(ret));
     return false;
 }
 
