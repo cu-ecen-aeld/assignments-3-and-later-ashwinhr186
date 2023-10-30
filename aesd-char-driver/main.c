99008f6bdd6e6c14bf065018542ba31c17fbe9e0
ashwinhr186 <ashwinhr186@Ubuntu.myguest.virtualbox.org>
2023-10-29 19:49:00 -0600

    drivertest works without and diff output

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -27,6 +27,7 @@ MODULE_AUTHOR("Ashwin Ravindra"); /** TODO: fill in your name **/
 MODULE_LICENSE("Dual BSD/GPL");
 
 struct aesd_dev aesd_device;
+char* final_buffptr = NULL;
 
 int aesd_open(struct inode *inode, struct file *filp)
 {
@@ -79,32 +80,53 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
 ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
                 loff_t *f_pos)
 {
-    ssize_t retval = -ENOMEM;
     PDEBUG("write %zu bytes with offset %lld",count,*f_pos);
+    ssize_t retval = -ENOMEM;
+    static size_t final_count = 0;
+    char* temp_buffptr = kmalloc(count, GFP_KERNEL);
+    copy_from_user(temp_buffptr, buf, count);
+    PDEBUG("temp_buffptr: %s", temp_buffptr);
+    if(strchr(temp_buffptr, '\n') == NULL) {
+        final_buffptr = krealloc(final_buffptr, final_count + count, GFP_KERNEL);
+        memcpy(final_buffptr + final_count, temp_buffptr, count);
+        PDEBUG("final_buffptr: %s", final_buffptr);
+        final_count += count;
+        retval = count;
+        kfree(temp_buffptr);
+        temp_buffptr = NULL;
+        return retval;
+    }
     /**
      * TODO: handle write
      */
+
+    /*If newline character found, append to final_buffptr and write to buffer*/
+    final_buffptr = krealloc(final_buffptr, final_count + count, GFP_KERNEL);
+    memcpy(final_buffptr + final_count, temp_buffptr, count);
+    PDEBUG("final_buffptr: %s", final_buffptr);
+    final_count += count;
+
     struct aesd_buffer_entry *new_entry = kmalloc(sizeof(struct aesd_buffer_entry), GFP_KERNEL);
     if (new_entry == NULL) {
         retval = -ENOMEM;
     }
-    else if((new_entry->buffptr = kmalloc(count, GFP_KERNEL)) == NULL) {
-        retval = -ENOMEM;
-    }
     else {
-        new_entry->size = count;
-        copy_from_user(new_entry->buffptr, buf, count);
+        new_entry->size = final_count;
+        new_entry->buffptr = kmalloc(final_count, GFP_KERNEL);
+        memcpy(new_entry->buffptr, final_buffptr, final_count);
         PDEBUG("buffer->in_offs before write: %d", aesd_device.command_buffer->in_offs);
         PDEBUG("write string: %s", new_entry->buffptr);
         mutex_lock(&aesd_device.write_lock);
         const char* overwritten_buffptr = aesd_circular_buffer_add_entry(aesd_device.command_buffer, new_entry);
         mutex_unlock(&aesd_device.write_lock);
         PDEBUG("buffer->in_offs after write: %d", aesd_device.command_buffer->in_offs);
-        if(overwritten_buffptr != NULL)
+        if(overwritten_buffptr != NULL) {
+            PDEBUG("overwritten_buffptr: %s", overwritten_buffptr);
             kfree(overwritten_buffptr);
+        }
         retval = count;
+        final_count = 0;
     }
-
     return retval;
 }
 struct file_operations aesd_fops = {
@@ -152,6 +174,7 @@ int aesd_init_module(void)
     aesd_device.command_buffer->in_offs = 0;
     aesd_device.command_buffer->out_offs = 0;
     aesd_device.command_buffer->full = false;
+    final_buffptr = kmalloc(4, GFP_KERNEL);
 
     result = aesd_setup_cdev(&aesd_device);
 
